bb1c08f6ad21e3cffae502865328016fcab0af23
Azzzll <coolpotcanchik@gmail.com>
2025-04-06 16:51:02 +0400

    added phpmyadmin, converted sql request to db

---
+++ phpmyadmin/tmp/twig/08/083a6b93a3fa396dc3ddc6cce5bf053865921af100e85534461480f6ca01dacd.php
@@ -0,0 +1,82 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* server/import/index.twig */
+class __TwigTemplate_20459d97f2899ab6fc732043bd5c3a95d459e98b011748034516bf3b02451f76 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "import.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("import.twig", "server/import/index.twig", 1);
+        yield from $this->parent->unwrap()->yield($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+yield _gettext("Importing into the current server");
+        return; yield '';
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "server/import/index.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  47 => 3,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "server/import/index.twig", "C:\\OSPanel\\home\\projects\\systemITS\\phpmyadmin\\templates\\server\\import\\index.twig");
+    }
+}
