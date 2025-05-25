d2ae05df86906beef25804fb9cd6cf8278c9723b
Azzzll <coolpotcanchik@gmail.com>
2025-05-25 15:43:40 +0400

    various connect types

--- scripts/db_equipment.php
+++ scripts/db_equipment.php
@@ -1,10 +1,22 @@
 <?php
 
+// Ихъяев
+
 $server = 'MySQL-8.0'; 
 $user = 'root';
 $password = '';
 $db_name = 'its_db';
 $link = mysqli_connect($server, $user, $password, $db_name);
+
+
+// Цепова
+/*
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'its_db';
+$link = mysqli_connect($host, $username, $password, $database);
+*/
 
 if (!$link) {
     die("Ошибка подключения: " . mysqli_connect_error());
